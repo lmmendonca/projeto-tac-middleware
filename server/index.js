03bc66b33a5ed165648e108b8d117c6b6110391e
lmmendonca <lmaximino.mendonca@gmail.com>
2020-06-22 23:42:34 -0300

    Implementacao da V2.0 inicial

--- server/index.js
+++ server/index.js
@@ -3,10 +3,16 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const GameController = require('./src/GameController')
+const GameController = require('./src/GameController');
 
 const SERVER_PORT = 3000;
 const app = express();
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+io.on('connection', (client) => {
+  console.log('conectado!');
+});
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -15,11 +21,9 @@ app.use((req, res, next) => {
   next();
 });
 
-const gameController = new GameController();
+const gameController = new GameController({ io });
 app.get('/start', gameController.start.bind(gameController));
 app.get('/status', gameController.getStatus.bind(gameController));
-app.get('/play', gameController.play.bind(gameController))
+app.get('/play', gameController.play.bind(gameController));
 
-app.listen(SERVER_PORT, function () {
-  console.log(`Server running on: http://localhost:${SERVER_PORT}`);
-});
+server.listen(SERVER_PORT);
